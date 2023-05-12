9a094ce97f50bb78ed40bbec44bc143683452af2
Станислав Потебенко <@itstep.krg>
2023-05-12 20:51:35 +0600

    JsControl

---
+++ static/js/food-band.js
@@ -0,0 +1,25 @@
+const par = document.querySelector('#food-band')
+const newElement=document.createElement('div')
+const url = "../food-band.json";
+const req = new XMLHttpRequest();
+req.open("GET", url);
+req.responseType="json";
+req.onload = () => {
+    console.log(req.response);
+    let arrMenu = req.response
+
+    for(let i of arrMenu)
+    {
+        par.innerHTML += `
+        <div class="card">
+        <div class="img" style="background-image: url(../${i.image})"></div>
+        <div class="block">
+        <h1>${i.name}</h1>
+        <p>Состав: ${i.description}</p>
+        <p>Цена: ${i.price}</p>
+        </div>
+        </div>
+        `
+    }
+}
+req.send();
